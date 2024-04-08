2b4bba528a63d131f30e5b3055381b8201805183
Nsralla <nsrhassan22@gmail.com>
2024-04-08 16:57:40 +0300

    add routers to app.jsx

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,5 +1,24 @@
 import './App.css'
 import MainPage from './components/MainPage'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import RightSection from './components/RightSection';
+import Alltodos from '../src/Pages/Alltodos.jsx';
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<MainPage/>,
+
+    children:[
+      {
+      path:'/addTodo',
+      element:<RightSection/>,
+    },
+    {
+      path:'/displayAll',
+      element:<Alltodos/>
+    }]
+  },
+]);
 function App() {
   return (
     <div className='mainPageDiv'>
